018ff0b9530b41eee00888cf3d8421b9f30f9c5d
Dima Koss <koss@null.net>
2022-09-24 02:55:08 +0300

    i don't remember what I did

--- pkg/fnmod/any.go
+++ pkg/fnmod/any.go
@@ -1,7 +1,5 @@
 package fnmod
 
-type Empty func()
-
 type Any func(...any) any
 
 type AnyIn func(...any)
--- pkg/stream/operation.go
+++ pkg/stream/operation.go
@@ -1,30 +1,43 @@
 package stream
 
 import (
-	"github.com/google/uuid"
-
 	"github.com/koss-null/lambda/internal/tools"
-	"github.com/koss-null/lambda/pkg/funcmodel"
 )
 
 const (
 	minSplitLen = 1024
 )
 
+type opType string
+
+const (
+	OpTypeMap      opType = "map"
+	OpTypeReduce          = "reduce"
+	OpTypeFilter          = "filter"
+	OpTypeSplit           = "split"
+	OpTypeSort            = "sort"
+	OpTypeSkip            = "skip"
+	OpTypeTrim            = "trim"
+	OpTypeSum             = "sum"
+	OpTypeContains        = "contains"
+	OpTypeGo              = "go"
+)
+
+var singleThreadOps = map[opType]struct{}{
+	OpTypeReduce: {},
+	OpTypeSkip:   {},
+	OpTypeTrim:   {},
+	OpTypeGo:     {},
+}
+
 type Operation[T any] struct {
-	id uuid.UUID
-	// sync if true, all op runs in single goroutine
-	sync bool
-	fn   func(dt []T, bm *tools.Bitmask[T])
-	// finished executes after the operation was finished
-	finished funcmodel.Empty
+	GrtCnt uint
+
+	op opType
+	fn func(dt []T, bm *tools.Bitmask[T])
 }
 
 // Do runs operation on n goroutines
 func (op *Operation[T]) Do(dt []T, bm *tools.Bitmask[T]) {
 	op.fn(dt, bm)
-}
-
-func (op *Operation[T]) Sync() bool {
-	return op.sync
 }
