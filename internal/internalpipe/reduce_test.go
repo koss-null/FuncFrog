c9ca1b664b63eb99654cfacad6ef0c2fa18f8243
Dima Kossovich <koss@null.net>
2023-07-28 02:29:30 +0300

    add internalpipe Sort tests

--- internal/internalpipe/reduce_test.go
+++ internal/internalpipe/reduce_test.go
@@ -1,1 +1,64 @@
 package internalpipe
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Reduce(t *testing.T) {
+	t.Parallel()
+
+	t.Run("single thread lim set", func(t *testing.T) {
+		p := Pipe[int]{
+			Fn: func(i int) (*int, bool) {
+				return &i, false
+			},
+			Len:           100_000,
+			ValLim:        -1,
+			GoroutinesCnt: 1,
+		}
+		res := p.Reduce(func(x, y *int) int { return *x + *y })
+		require.Equal(t, 4999950000, *res)
+	})
+
+	t.Run("seven thread lim set", func(t *testing.T) {
+		p := Pipe[int]{
+			Fn: func(i int) (*int, bool) {
+				return &i, false
+			},
+			Len:           100_000,
+			ValLim:        -1,
+			GoroutinesCnt: 7,
+		}
+		res := p.Reduce(func(x, y *int) int { return *x + *y })
+		require.Equal(t, 4999950000, *res)
+	})
+
+	t.Run("single thread ValLim set", func(t *testing.T) {
+		p := Pipe[int]{
+			Fn: func(i int) (*int, bool) {
+				return &i, false
+			},
+			Len:           -1,
+			ValLim:        100_000,
+			GoroutinesCnt: 1,
+		}
+		res := p.Reduce(func(x, y *int) int { return *x + *y })
+		require.Equal(t, 4999950000, *res)
+	})
+
+	t.Run("seven thread ValLim set", func(t *testing.T) {
+		p := Pipe[int]{
+			Fn: func(i int) (*int, bool) {
+				return &i, false
+			},
+			Len:           -1,
+			ValLim:        100_000,
+			GoroutinesCnt: 7,
+		}
+
+		res := p.Reduce(func(x, y *int) int { return *x + *y })
+		require.Equal(t, 4999950000, *res)
+	})
+}
